c773b04f4940339776f7215b82236988d0c93cb9
Max Heimbrock <maxheimbrock@nianticlabs.com>
2022-09-20 10:37:04 +0200

    build from extern folder

--- apps/Main.cpp
+++ apps/Main.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
 #include "crow.h"
+// #include "../extern/Crow/include/crow.h" for using extern header include folder
 
 int main()
 {
-    std::cout << "Hello Max from cpp" << std::endl;
-    crow::SimpleApp app;
+    crow::SimpleApp app; //define your crow application
+
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/test/")([](){
+        return "Hello Max from Crow";
+    });
+
+    //set the port, set the app to run on multiple threads, and run the app
+    app.port(18080).multithreaded().run();
 }
